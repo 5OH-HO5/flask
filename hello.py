fd2740f5c066a1fdcc89277476d7594b7798a406
5OH-HO5 <omj0414@daum.net>
2019-12-12 10:28:42 +0900

    html로 응답하기

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
+# 요청이 들어오면 return해준다
 # 플라스크 모듈 안에서 import 3개 불러오기
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template
 # 플라스크를 app이라는 변수에 저장
 app = Flask(__name__)
 # 밑에 있는 함수가 시작되기 전에 시작되는 부분
@@ -19,6 +20,14 @@ def hi():
 # 127.0.0.1:5000/oh
 def oh():
     return 'ohminjae'
+
+@app.route(/html_tag)
+def html_tag():
+    return '<h1>안녕하세요</h1>'
+
+@app.route(html_file)
+def html_file():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
