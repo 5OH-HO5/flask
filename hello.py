39fc8ac1484ff5efde90c92b7076a0e190f153a3
5OH-HO5 <omj0414@daum.net>
2019-12-12 10:16:59 +0900

    route와 함수 관계 배우기

--- hello.py
+++ hello.py
@@ -1,11 +1,24 @@
+# 플라스크 모듈 안에서 import 3개 불러오기
 from flask import Flask, escape, request
-
+# 플라스크를 app이라는 변수에 저장
 app = Flask(__name__)
-
+# 밑에 있는 함수가 시작되기 전에 시작되는 부분
+# /라는 요청이 들어오면 밑에있는 hello함수를 실행시키겠다
 @app.route('/')
+# /라는 것은 127.0.0.1:5000이 숨겨져 있다
 def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
+
+@app.route('/hi')
+# 127.0.0.1:5000/hi
+def hi():
+    return 'hi'
+
+@app.route('/oh')
+# 127.0.0.1:5000/oh
+def oh():
+    return 'ohminjae'
 
 if __name__ == '__main__':
     app.run(debug=True)
